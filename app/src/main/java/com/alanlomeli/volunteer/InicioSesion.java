b09576553aeaa230d463c0b9cd5b9b161e397872
Juan Pablo Rodríguez Gutiérrez <juanpa1099@hotmail.com>
2019-04-24 13:56:25 -0500

    Barra de navegacion del activity main con nombres, cambiar color y agregar posibles items

--- app/src/main/java/com/alanlomeli/volunteer/InicioSesion.java
+++ app/src/main/java/com/alanlomeli/volunteer/InicioSesion.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InicioSesion extends AppCompatActivity {
-
+//nito
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
