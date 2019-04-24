fc20a4fc657d5453b9772880c632585558a64208
Juan Pablo Rodríguez Gutiérrez <juanpa1099@hotmail.com>
2019-04-24 12:58:20 -0500

    Barra de navegacion del activity main con nombres

--- app/src/main/java/com/alanlomeli/volunteer/MainActivity.java
+++ app/src/main/java/com/alanlomeli/volunteer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.alanlomeli.volunteer;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Wa
+        //
     }
 }
