9288ec114aab5edf655ea254134f7856243fb192
deadchri5 <deadchri5h@gmail.com>
2019-04-24 17:31:39 -0500

    Actividad de Inicion de sesion corregido, ya me manda a al activity main

--- app/src/main/java/com/alanlomeli/volunteer/InicioSesion.java
+++ app/src/main/java/com/alanlomeli/volunteer/InicioSesion.java
@@ -1,16 +1,26 @@
 package com.alanlomeli.volunteer;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class InicioSesion extends AppCompatActivity {
 //nito
+    Button Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+        Login = findViewById(R.id.btnLogin);
+    }
+
+    public void btnIniciar (View view){
+        Intent mostrarInicio = new Intent(this,MainActivity.class);
+        startActivity(mostrarInicio);
     }
 }
